fbf78d3feec0640c24df419997a57ea4376355af
RomanKRA44 <adm-nppl@yandex.ru>
2022-04-05 18:54:32 +0300

    ДЗ 3  к уроку 4

--- HWLEsson4/Program.cs
+++ HWLEsson4/Program.cs
@@ -1,13 +1,54 @@
 ﻿using System;
-using System.Linq;
 
-namespace HWLEsson4
+namespace HW_Lesson_4_3
 {
     class Program
     {
+        enum season { Error, Winter, Spring, Summer, Autumn }
+        static season OfMonth(int n)
+        {
+            try
+            {
+                if (n <= 0 || n > 12)
+                    throw new Exception("Ошибка: введите число от 1 до 12");
+                switch ((n % 12) / 3)
+                {
+                    case 0:
+                        return season.Winter;
+                    case 1:
+                        return season.Spring;
+                    case 2:
+                        return season.Summer;
+                    default: return season.Autumn;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return season.Error;
+            }
+        }
+        static string Season(season s)
+        {
+            switch (s)
+            {
+                case season.Winter:
+                    return "Зима";
+                case season.Spring:
+                    return "Весна";
+                case season.Summer:
+                    return "Лето";
+                case season.Autumn:
+                    return "Осень";
+                default: return "";
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).Sum());
+            do
+            {
+                Console.WriteLine("Введите номер месяца: ");
+                Console.WriteLine(Season(OfMonth(Convert.ToInt32(Console.ReadLine()))));
+            }
+            while (true);
         }
-    }
-}
