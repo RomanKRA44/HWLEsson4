8b2c1ee1397da7d7a2c7dfa108ad7300e333963f
RomanKRA44 <adm-nppl@yandex.ru>
2022-04-05 18:53:02 +0300

    ДЗ 1 к уроку 4

--- HWLEsson4/Program.cs
+++ HWLEsson4/Program.cs
@@ -6,7 +6,15 @@ namespace HWLEsson4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(GetFullName("Вишневский", "Роман", "Петрович"));
+            Console.WriteLine(GetFullName("Есина", "Алена", "Ивановна"));
+            Console.WriteLine(GetFullName("Смирнова", "Илона", "Владимировна"));
+            Console.WriteLine(GetFullName("Смирнов", "Иван", "Иваныч"));
+        }
+
+        static string GetFullName(string lastName, string firstName, string patronymic)
+        {
+            return $"{lastName} {firstName} {patronymic}";
         }
     }
 }
